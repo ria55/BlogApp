e2990756c4354628dbe6dcc31133e24c2d59a472
Ria <benedek.maria88@gmail.com>
2021-10-28 14:23:12 +0200

    started to implement UserController's endpoints, also refactored UserService

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,6 +1,10 @@
 package app.controllers;
 
+import app.models.AppUser;
 import app.returnModels.Feedback;
+import app.returnModels.ObjectBack;
+import app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -8,14 +12,36 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
+    private UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @PostMapping("/register")
-    public Feedback registerUser() {
-        return new Feedback(false, HttpStatus.BAD_GATEWAY);
+    public Feedback registerUser(@RequestBody AppUser user) {
+        Feedback feedback;
+
+        try {
+            AppUser registered = service.registerUser(user);
+            feedback = new ObjectBack<>(registered);
+        } catch (Exception e) {
+            feedback = new Feedback(false, HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        return feedback;
     }
 
     @GetMapping("/user")
     public Feedback getLoggedInUser() {
-        return new Feedback(false, HttpStatus.BAD_GATEWAY);
+        AppUser user = service.getLoggedInUser();
+
+        if (user != null) {
+            return new ObjectBack<>(user);
+        }
+
+        return new Feedback(false, HttpStatus.BAD_REQUEST, "no user logged in");
     }
 
     @PutMapping("/user")
--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -2,10 +2,16 @@ package app.services;
 
 import app.jpaRepos.UserRepo;
 import app.models.AppUser;
+import app.returnModels.Feedback;
+import app.returnModels.ObjectBack;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,41 +20,48 @@ import java.util.List;
 @Service
 public class UserService extends ServiceBase implements UserDetailsService {
 
-    private UserRepo userRepo;
+    private PasswordEncoder encoder;
 
     @Autowired
-    public UserService(UserRepo userRepo) {
-        this.userRepo = userRepo;
+    public UserService(PasswordEncoder encoder) {
+        this.encoder = encoder;
     }
-
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser user = em.createQuery(
+        return em.createQuery(
                 "SELECT user FROM AppUser user WHERE user.username = :name",
                 AppUser.class)
                 .setParameter("name", username)
                 .getSingleResult();
+    }
 
-        // em.find(AppUser.class, username);
+    public AppUser getLoggedInUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        return user;
+        if (auth != null) {
+            Object user = auth.getPrincipal();
+            if (user instanceof AppUser) {
+                return (AppUser) user;
+            }
+        }
+
+        return null;
     }
 
     @Transactional
     public List<AppUser> listAllUsers() {
-        List<AppUser> users = em.createQuery("SELECT user FROM AppUser user", AppUser.class)
+        return em.createQuery("SELECT user FROM AppUser user", AppUser.class)
                 .getResultList();
-
-        List<AppUser> users2 = userRepo.findAll();
-
-        return users;
     }
 
     @Transactional
-    public void registerUser(AppUser user) {
-        userRepo.save(user);
+    public AppUser registerUser(AppUser user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        em.persist(user);
+
+        return (AppUser) loadUserByUsername(user.getUsername());
     }
 
     @Transactional
